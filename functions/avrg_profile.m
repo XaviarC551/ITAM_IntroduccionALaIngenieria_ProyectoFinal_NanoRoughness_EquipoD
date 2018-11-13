4c5d6600263467642e42cb3d0965aa77da55356c
xaviarazor <prietojavi551@gmail.com>
2018-11-13 12:21:08 -0600

    avrg_profile_13_11_18

---
+++ functions/avrg_profile.m
@@ -0,0 +1,12 @@
+function [rz] = avrg_profile(data, p, v)
+    sorted=sort(data);
+    s=0;
+    l=length(sorted)
+    for i=0:p-1
+       s=s+sorted(l-i);
+    end
+    for i=1:v
+        s=s+sorted(i);
+    end
+    rz=s/(p+v);
+end
