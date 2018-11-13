9ecb849f3e63a7e59a33b646fa45de0d40fccfda
xaviarazor <prietojavi551@gmail.com>
2018-11-13 11:32:35 -0600

    optimizacion_!_13_11_18

--- example_scripts/show_cuts.m
+++ example_scripts/show_cuts.m
@@ -1,4 +1,7 @@
+close all;
+clear;clc;
 AMF = imread('img/Imagen4AMFFinal.gif');
+
 h=length(AMF(:,1));
 w=length(AMF);
 
@@ -34,15 +37,28 @@ dirY=mod(d+1,3)-1
 dirX=mod(d,2)
 
 datos=[];
+fx=x;
+fy=y;
 for i=0:(randi(137))
-    if ((x+dirX*i) > w || (y+dirY*i) > h || (y+dirY*i) < 1) 
+    if ((x+dirX*i) > w || (y+dirY*i) > h || (y+dirY*i) < 1)
         break
     end  
     
     datos(i+1) = AMF(y+dirY*i,x+dirX*i);
+    x=x+dirX*i;
+    y=y+dirY*i;
 end
 xg = 1:length(datos);
 yg = datos;
 plot(xg,yg)
 axis([1,length(datos)+1,0,255])
 grid on
+
+figure
+imshow(AMF);
+
+figure
+imshow(AMF);
+hold on
+plot([x fx],[y fy],'r','LineWidth',2);
+hold off
